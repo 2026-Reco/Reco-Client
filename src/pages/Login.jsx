9ce5078f13a2b765752a8992f5efd4025c404c85
최소영 <soyoungchoi791@gmail.com>
2026-04-27 00:53:21 +0900

    feat: 로그인 회원가입 및 검색 기능 구현

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,112 +1,133 @@
-    import InputLogin from "../components/InputLogin.jsx";
-    import SnsLogin from "../components/SnsLogin.jsx";
-    import { useState } from "react"
-    import styled from "styled-components"
-    import { Link } from "react-router-dom"
+import InputLogin from "../components/InputLogin.jsx";
+import SnsLogin from "../components/SnsLogin.jsx";
+import { useState } from "react"
+import styled from "styled-components"
+import { Link, useNavigate } from "react-router-dom"
 
-    const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    padding: 40px 24px;
-    gap: 16px;
-    
-    color: #272727;
-    font-family: 'Paperlogy';
-    font-size: 14px;
-    font-weight: 600;
-    line-height: 20px;
-    margin-top: 130px;
-    `
-    const SubTitle = styled.p`
-    margin-bottom: 5px;  /* ← 숫자 조절하면서 맞춰봐요 */
-    text-align: left;
-    /*//  padding: 40px 30px; */
-    `
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 40px 24px;
+  gap: 16px;
+  color: #272727;
+  font-family: 'Paperlogy';
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 20px;
+  margin-top: 150px;
+`
 
-    const Title = styled.p`
-    font-family: 'Paperlogy';
-    font-size: 24px;
-    font-weight: 600;
-    line-height: 20px;
-    color: #272727;
-    margin-bottom: 30px;
-    text-align: left;
-    `
+const SubTitle = styled.p`
+  margin-bottom: 5px;
+  text-align: left;
+`
 
-    const GreenText = styled.span`
-    color: #53B175;
-    `
-    
+const Title = styled.p`
+  font-family: 'Paperlogy';
+  font-size: 24px;
+  font-weight: 600;
+  line-height: 20px;
+  color: #272727;
+  margin-bottom: 30px;
+  text-align: left;
+`
 
-    const Button = styled.button`
-    width: 100%;
-    height: 54px;
-    border-radius: 10px;
-    background: #53B175;
-    border: none;
-    color: white;   
-    font-family: 'Paperlogy';
-    font-size: 16px;
-    cursor: pointer;
-    margin-top: 25px;
-    border-radius: 10px;
-    
-    `
-    const LoginText = styled.p`
-    font-family: 'Paperlogy';
-    font-size: 11px;
-    font-weight: 400;
-    line-height: 20px;
-    color: rgba(0, 0, 0, 0.80);
-    text-align: center;
-    text-align: left;
-    
-    `
+const GreenText = styled.span`
+  color: #53B175;
+`
 
-    const LoginLink = styled.span`
-    font-family: 'Paperlogy';
-    font-size: 11px;
-    font-weight: 500;
-    line-height: 20px;
-    color: rgba(100, 178, 148, 0.80);
-    cursor: pointer;
-    `
+const Button = styled.button`
+  width: 100%;
+  height: 54px;
+  border-radius: 10px;
+  background: #53B175;
+  border: none;
+  color: white;
+  font-family: 'Paperlogy';
+  font-size: 16px;
+  cursor: pointer;
+  margin-top: 25px;
+  opacity: ${({ disabled }) => (disabled ? 0.5 : 1)};  /* 비활성화시 흐리게 */
+`
 
-    const SignUp = () => {
-    const [userId, setUserId] = useState("")
-    const [password, setPassword] = useState("")
-    const [passwordCheck, setPasswordCheck] = useState("")
+const LoginText = styled.p`
+  font-family: 'Paperlogy';
+  font-size: 11px;
+  font-weight: 400;
+  line-height: 20px;
+  color: rgba(0, 0, 0, 0.80);
+  text-align: left;
+`
 
-    return (
-        <Container>
-        <div>
-            <SubTitle>다시 만나서 반갑습니다!</SubTitle>
-            <Title>3초만에 <GreenText>로그인</GreenText> 완료하기</Title>
-        </div>
+const LoginLink = styled.span`
+  font-family: 'Paperlogy';
+  font-size: 11px;
+  font-weight: 500;
+  line-height: 20px;
+  color: rgba(100, 178, 148, 0.80);
+  cursor: pointer;
+`
 
-        <InputLogin
-            label="아이디"
-            placeholder="아이디를 입력하시오"
-            type="text"
-            value={userId}
-            onChange={(e) => setUserId(e.target.value)}
-        />
-        <InputLogin
-            label="비밀번호"
-            placeholder="비밀번호를 입력하시오"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-        />
+const ErrorMsg = styled.p`
+  font-size: 11px;
+  color: red;
+  font-family: 'Paperlogy';
+`
 
-        <Button>로그인</Button>
-        <LoginText>
-            아직 회원이 아니신가요? <Link to="/signup"><LoginLink>회원가입</LoginLink></Link>
-        </LoginText>
-        <SnsLogin />
-        </Container>
-    )
+const Login = () => {
+  const navigate = useNavigate()
+  const [userId, setUserId] = useState("")
+  const [password, setPassword] = useState("")
+  const [errorMsg, setErrorMsg] = useState("")
+
+  const handleLogin = () => {
+    // 빈값 검사
+    if (!userId || !password) {
+      setErrorMsg("아이디와 비밀번호를 입력해주세요.")
+      return
     }
+    setErrorMsg("")
+    // 나중에 여기서 API 호출
+    navigate("/")  // 성공시 메인으로 이동
+  }
 
-    export default SignUp
+  return (
+    <Container>
+      <div>
+        <SubTitle>다시 만나서 반갑습니다!</SubTitle>
+        <Title>3초만에 <GreenText>로그인</GreenText> 완료하기</Title>
+      </div>
+
+      <InputLogin
+        label="아이디"
+        placeholder="아이디를 입력하시오"
+        type="text"
+        value={userId}
+        onChange={(e) => setUserId(e.target.value)}
+      />
+      <InputLogin
+        label="비밀번호"
+        placeholder="비밀번호를 입력하시오"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      {errorMsg && <ErrorMsg>{errorMsg}</ErrorMsg>}
+
+      <Button
+        onClick={handleLogin}
+        disabled={!userId || !password}
+      >
+        로그인
+      </Button>
+      <LoginText>
+        아직 회원이 아니신가요? <Link to="/signup"><LoginLink>회원가입</LoginLink></Link>
+      </LoginText>
+      <SnsLogin />
+    </Container>
+  )
+}
+
+export default Login
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,10 +1,10 @@
-    import InputLogin from "../components/InputLogin.jsx";
-    import SnsLogin from "../components/SnsLogin.jsx";
-    import { useState } from "react"
-    import styled from "styled-components"
-    import { Link } from "react-router-dom"
+import InputLogin from "../components/InputLogin.jsx";
+import SnsLogin from "../components/SnsLogin.jsx";
+import { useState } from "react"
+import styled from "styled-components"
+import { Link } from "react-router-dom"
 
-    const Container = styled.div`
+const Container = styled.div`
     display: flex;
     flex-direction: column;
     width: 100%;
@@ -16,15 +16,16 @@
     font-size: 14px;
     font-weight: 600;
     line-height: 20px;
-    margin-top: 60px;
+    margin-top: 120px;
+    
     `
-    const SubTitle = styled.p`
+const SubTitle = styled.p`
     margin-bottom: 5px;  /* ← 숫자 조절하면서 맞춰봐요 */
     text-align: left;
     /*//  padding: 40px 30px; */
     `
 
-    const Title = styled.p`
+const Title = styled.p`
     font-family: 'Paperlogy';
     font-size: 24px;
     font-weight: 600;
@@ -34,12 +35,12 @@
     text-align: left;
     `
 
-    const GreenText = styled.span`
+const GreenText = styled.span`
     color: #53B175;
     `
-    
 
-    const Button = styled.button`
+
+const Button = styled.button`
     width: 100%;
     height: 54px;
     border-radius: 10px;
@@ -52,7 +53,7 @@
     margin-top: 25px;
     border-radius: 10px;
     `
-    const LoginText = styled.p`
+const LoginText = styled.p`
     font-family: 'Paperlogy';
     font-size: 11px;
     font-weight: 400;
@@ -62,7 +63,7 @@
     text-align: left;
     `
 
-    const LoginLink = styled.span`
+const LoginLink = styled.span`
     font-family: 'Paperlogy';
     font-size: 11px;
     font-weight: 500;
@@ -70,48 +71,68 @@
     color: rgba(100, 178, 148, 0.80);
     cursor: pointer;
     `
+const ErrorMsg = styled.p`
+  font-size: 11px;
+  color: red;
+  font-family: 'Paperlogy';
+`
 
-    const SignUp = () => {
+const SignUp = () => {
     const [userId, setUserId] = useState("")
     const [password, setPassword] = useState("")
     const [passwordCheck, setPasswordCheck] = useState("")
+    const [errorMsg, setErrorMsg] = useState("")
+
+    const handleSignUp = () => {
+        if (!userId || !password || !passwordCheck) {
+            setErrorMsg("모든 항목을 입력해주세요.")
+            return
+        }
+        if (password !== passwordCheck) {
+            setErrorMsg("비밀번호가 일치하지 않습니다.")
+            return
+        }
+        setErrorMsg("")
+        // 나중에 여기서 API 호출
+    }
 
     return (
         <Container>
-        <div>
-            <SubTitle>안녕하세요, 처음 만나서 반가워요!</SubTitle>
-            <Title>3초만에 <GreenText>회원가입</GreenText> 완료하기</Title>
-        </div>
+            <div>
+                <SubTitle>안녕하세요, 처음 만나서 반가워요!</SubTitle>
+                <Title>3초만에 <GreenText>회원가입</GreenText> 완료하기</Title>
+            </div>
 
-        <InputLogin
-            label="아이디"
-            placeholder="아이디를 입력하시오"
-            type="text"
-            value={userId}
-            onChange={(e) => setUserId(e.target.value)}
-        />
-        <InputLogin
-            label="비밀번호"
-            placeholder="비밀번호를 입력하시오"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-        />
-        <InputLogin
-            label="비밀번호 확인"   
-            placeholder="비밀번호를 확인하시오"
-            type="password"
-            value={passwordCheck}
-            onChange={(e) => setPasswordCheck(e.target.value)}
-        />
+            <InputLogin
+                label="아이디"
+                placeholder="아이디를 입력하시오"
+                type="text"
+                value={userId}
+                onChange={(e) => setUserId(e.target.value)}
+            />
+            <InputLogin
+                label="비밀번호"
+                placeholder="비밀번호를 입력하시오"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <InputLogin
+                label="비밀번호 확인"
+                placeholder="비밀번호를 확인하시오"
+                type="password"
+                value={passwordCheck}
+                onChange={(e) => setPasswordCheck(e.target.value)}
+            />
 
-        <Button>회원가입</Button>
-        <LoginText>
-            이미 계정이 있으신가요? <Link to="/login"><LoginLink>로그인</LoginLink></Link>
-        </LoginText>
-        <SnsLogin />
+            <Button onClick={handleSignUp}>회원가입</Button>
+            {errorMsg && <ErrorMsg>{errorMsg}</ErrorMsg>}
+            <LoginText>
+                이미 계정이 있으신가요? <Link to="/login"><LoginLink>로그인</LoginLink></Link>
+            </LoginText>
+            <SnsLogin />
         </Container>
     )
-    }
+}
 
-    export default SignUp
+export default SignUp
